b6188763b13e590b159af74ea0496c232d7b08ae
ankkitt19 <65223550+ankkitt19@users.noreply.github.com>
2021-10-07 13:38:00 +0530

    Create MiddleofLL.cpp

---
+++ MiddleofLL.cpp
@@ -0,0 +1,64 @@
+Method 1:
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+        int len=0;
+        ListNode *curr=head;
+        while(curr!=NULL){
+            curr=curr->next;
+            ++len;
+        }
+        int toprint=0;
+        if(len%2==0){
+            toprint=len/2-1;
+        }
+        else{
+            toprint=(len/2)+1;
+            toprint-=2;
+        }
+        curr=head;
+        while(toprint>=0){
+            curr=curr->next;
+            --toprint;
+        }
+        return curr;
+    }
+};
+
+
+
+Method 2:
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+        ListNode* slow=head;
+        ListNode* fast=head;
+        while(fast!=NULL && fast->next!=NULL){
+            slow=slow->next;
+            fast=fast->next->next;
+        }
+        return slow;
+    }
+};
