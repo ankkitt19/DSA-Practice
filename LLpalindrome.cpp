06017dd368e32975be952df3ad2871e6c835806e
ankkitt19 <65223550+ankkitt19@users.noreply.github.com>
2021-10-11 12:16:45 +0530

    Update LLpalindrome.cpp

--- LLpalindrome.cpp
+++ LLpalindrome.cpp
@@ -1,3 +1,7 @@
+MEthod 1:
+
+
+
 /**
  * Definition for singly-linked list.
  * struct ListNode {
@@ -48,4 +52,58 @@ public:
         }
     return true;
     }
+};
+
+
+Method 2:
+
+
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseLL(ListNode* head){
+    
+        ListNode* prev=NULL;
+        ListNode* next=NULL;
+        ListNode* curr=head;
+        while(curr!=NULL){
+            
+            next=curr->next;
+            curr->next=prev;
+            prev=curr;
+            curr=next;
+        }
+        return prev;
+    }
+    bool isPalindrome(ListNode* head) {
+     if(head==NULL || head->next==NULL) {
+        return head; 
+     }  
+    ListNode* slow=head;
+    ListNode* fast=head;
+    while(fast->next!=NULL and fast->next->next!=NULL){
+        slow=slow->next;
+        fast=fast->next->next;
+    }
+    slow->next=reverseLL(slow->next);
+    slow=slow->next;
+    while(slow!=NULL){
+        if(head->val!=slow->val){
+            return false;
+        }
+        slow=slow->next;
+        head=head->next;
+    }
+    return true;
+}
 };
