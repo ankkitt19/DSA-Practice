8641ff354eb57fe266f362ffc28440bb117737cf
ankkitt19 <65223550+ankkitt19@users.noreply.github.com>
2022-07-06 12:10:58 +0530

    Time: 30 ms (17.68%), Space: 22.1 MB (5.18%) - LeetHub

--- 110-balanced-binary-tree/110-balanced-binary-tree.cpp
+++ 110-balanced-binary-tree/110-balanced-binary-tree.cpp
@@ -1,40 +1,44 @@
 /**
  *Definition for a binary tree node.
  *struct TreeNode {
- *    int val;
- *    TreeNode * left;
- *    TreeNode * right;
- *    TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *   int val;
+ *   TreeNode * left;
+ *   TreeNode * right;
+ *   TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *   TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *   TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  *};
  */
 class Solution
 {
     public:
-        int height(TreeNode *root)
+        pair<bool, int> fast(TreeNode *root)
         {
             if (root == NULL)
             {
-                return 0;
+                pair<bool, int> p = make_pair(true, 0);
+                return p;
             }
-            int lh = height(root->left);
-            int rh = height(root->right);
-            return max(lh, rh) + 1;
+            pair<bool, int> left = fast(root->left);
+            pair<bool, int> right = fast(root->right);
+            bool l = left.first;
+            bool r = right.first;
+            pair<bool, int> ans;
+
+            bool diff = abs(left.second - right.second) <= 1;
+            ans.second = max(left.second, right.second) + 1;
+            if (l and r and diff)
+            {
+                ans.first = true;
+            }
+            else
+            {
+                ans.first = false;
+            }
+            return ans;
         }
     bool isBalanced(TreeNode *root)
     {
-        if (root == NULL)
-        {
-            return true;
-        }
-        bool left = isBalanced(root->left);
-        bool right = isBalanced(root->right);
-        bool diff = abs(height(root->left) - height(root->right)) <= 1;
-        if (left and right and diff)
-        {
-            return true;
-        }
-        return false;
+        return fast(root).first;
     }
 };
