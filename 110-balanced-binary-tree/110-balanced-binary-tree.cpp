6fbc771c422580d80b114fcf21cde1256a005f18
ankkitt19 <65223550+ankkitt19@users.noreply.github.com>
2022-07-06 11:59:12 +0530

    Time: 175 ms (5.07%), Space: 21.1 MB (14.92%) - LeetHub

--- 110-balanced-binary-tree/110-balanced-binary-tree.cpp
+++ 110-balanced-binary-tree/110-balanced-binary-tree.cpp
@@ -1,30 +1,40 @@
 /**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
+ *Definition for a binary tree node.
+ *struct TreeNode {
+ *    int val;
+ *    TreeNode * left;
+ *    TreeNode * right;
+ *    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *};
  */
-class Solution {
-public:
-    bool isBalanced(TreeNode* root) {
-       return check(root)!=-1;
+class Solution
+{
+    public:
+        int height(TreeNode *root)
+        {
+            if (root == NULL)
+            {
+                return 0;
+            }
+            int lh = height(root->left);
+            int rh = height(root->right);
+            return max(lh, rh) + 1;
+        }
+    bool isBalanced(TreeNode *root)
+    {
+        if (root == NULL)
+        {
+            return true;
+        }
+        bool left = isBalanced(root->left);
+        bool right = isBalanced(root->right);
+        bool diff = abs(height(root->left) - height(root->right)) <= 1;
+        if (left and right and diff)
+        {
+            return true;
+        }
+        return false;
     }
-    int check(TreeNode* root){
-        if(root==NULL){
-            return 0;
-        }
-        int lh=check(root->left);
-        if(lh==-1) return -1;
-        int rh=check(root->right);
-        if(rh==-1) return -1;
-        if(abs(lh-rh)>1){
-            return -1;
-        }
-        return 1+max(lh,rh);
-     }
 };
